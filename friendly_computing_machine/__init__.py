5cbfe3454c0c67e7ecd7bf6780c3ac0ec9506817
dcperera <charitha.perera@maine.edu>
2017-07-25 16:33:08 -0400

    blah

---
+++ friendly_computing_machine/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is the base file of the friendly-computing-machines!
+"""
+from . import math
+from .math  import mult
---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,19 @@
+"""
+A small set of functions for doing math operations
+"""
+
+# write a function named ass that adds two values
+
+def add(A,B):
+    """
+    Function that adds two variables
+    """
+    return A+B
+
+def mult(A, B):
+
+    """"
+    Function that multiply two variables
+    """
+    return A*B
+
